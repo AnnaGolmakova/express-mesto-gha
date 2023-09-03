f6f2c568807831c20c6248da8e4070ad260bda86
Anna Golmakova <ann.golmakova@gmail.com>
2023-09-03 18:42:11 +0500

    поправила вывод ошибок в API гыукы

--- controllers/users.js
+++ controllers/users.js
@@ -4,24 +4,84 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(400).send({
-      message: 'Не удалось создать пользователя',
-    }));
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Не удалось создать пользователя',
+        });
+      }
+      return res.status(500).send({
+        message: 'Произошла неизвестная ошибка',
+      });
+    });
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(404).send({
-      message: 'Пользователи не найдены',
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({
+      message: 'Не удалось получить пользователей',
     }));
 };
 
 module.exports.getUserById = (req, res) => {
-  User.findOne({ _id: req.params.id })
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(404).send({
-      message: 'Пользователь не найден',
-    }));
+  User.findOne({ _id: req.param._id })
+    .then((user) => {
+      if (user === null) {
+        return res.status(404).send({
+          message: 'Пользователь не найден',
+        });
+      }
+      return res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({
+          message: 'Неправильно передан ID пользователя',
+        });
+      }
+      return res.status(500).send({
+        message: 'Не удалось получить данные о пользователе',
+      });
+    });
+};
+
+module.exports.getMyUser = (req, res) => {
+  req.param._id = req.user._id;
+  this.getUserById(req, res);
+};
+
+module.exports.updateUserInfo = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about, avatar },
+    {
+      new: true,
+      runValidators: true,
+      upsert: false,
+    },
+  )
+    .then((user) => {
+      if (user === null) {
+        return res.status(404).send({
+          message: 'Пользователь не найден',
+        });
+      }
+      return res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res.status(400).send({
+          message: 'Неправильно переданы данные',
+        });
+      }
+      return res.status(500).send({ message: 'Произошла ошибка' });
+    });
+};
+
+module.exports.updateAvatar = (req, res) => {
+  this.updateUserInfo(req, res);
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 const router = require('express').Router();
-const { createUser, getUsers, getUserById } = require('../controllers/users');
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  getMyUser,
+  updateUserInfo,
+  updateAvatar,
+} = require('../controllers/users');
 
 router.post('/', createUser);
-
 router.get('/', getUsers);
+router.get('/me', getMyUser);
 router.get('/:id', getUserById);
+router.patch('/me', updateUserInfo);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
