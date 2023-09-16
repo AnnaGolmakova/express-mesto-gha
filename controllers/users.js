02ffca706a986972857b88c950647c4652cb317e
Anna Golmakova <ann.golmakova@gmail.com>
2023-09-16 15:31:39 +0500

    создала контроллер login

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,12 @@
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-const { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../constants');
+const {
+  BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR, UNAUTHORIZED,
+} = require('../constants');
 
 module.exports.createUser = (req, res) => {
   const {
@@ -142,5 +145,32 @@ module.exports.updateAvatar = (req, res) => {
         });
       }
       return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      const token = jwt.sign({ _id: matched._id }, 'some-secret-key', { expiresIn: '7d' });
+
+      res.cookie('token', token, { httpOnly: true });
+    })
+    .catch((err) => {
+      res
+        .status(UNAUTHORIZED)
+        .send({ message: err.message });
     });
 };
